aad0c900a964c3d5de4fe03ace4ae39f46774e8c
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-06 09:33:21 +0530

    Modified: To print diagramatic blocks and index

--- src/com/bridgelabz/tictactoe/TicTacToe.java
+++ src/com/bridgelabz/tictactoe/TicTacToe.java
@@ -33,14 +33,18 @@ public class TicTacToe {
 		}	
 	}
 	public static void printBoardFirstTime() {
-		
-		for(int index=1;index<10;index++) {
-			if(index%3==0)
-				System.out.println();
-			System.out.print(board[index]);
+
+		for(int index=1;index<=9;index++) {
+			if(index%3 == 0)
+				if(index == 3 || index ==6 || index==9)
+					System.out.println("|"+index+"|");
+				else 
+					System.out.println();
+			if(index%3!=0)
+				System.out.print("|"+index+"|	");
 		}
 	}
-	
+
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Tic Tac Toe program: ");
