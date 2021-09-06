ad40ba12f1f5a44fcca6d083d59a25ddd5ed5e34
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-06 08:48:04 +0530

    Added: To choose and assign character

--- src/com/bridgelabz/tictactoe/TicTacToe.java
+++ src/com/bridgelabz/tictactoe/TicTacToe.java
@@ -1,17 +1,43 @@
 package com.bridgelabz.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToe {
-	
-	public char[] board= new char[10];
-	
-	public void initializeBoard() {
+
+	public static Scanner scanner = new Scanner(System.in);
+	public static char[] board= new char[10];
+	public static char playerCharacter;
+	public static char computerCharacter;
+
+	public static void initializeBoard() {
+
 		for(int index = 1;index<10;index++) {
 			board[index] = ' ';
 		}
 	}
-	
-	public static void main(String[] args) {
-		
-	}
+	public static void chooseCharacter() {
+		System.out.print("Choose Player Character (X/O): ");
+		String myChoice = scanner.nextLine();
+		myChoice.toUpperCase();
+		if(myChoice.equalsIgnoreCase("X")) {
+			playerCharacter = 'X';
+			computerCharacter='O';
+		}
 
+		else if(myChoice.equalsIgnoreCase("O")) {
+			playerCharacter = 'O';
+			computerCharacter='X';
+		}
+		else {
+			System.out.println("Invalid Choice!");
+		}	
+	}
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Tic Tac Toe program: ");
+		System.out.println();
+		initializeBoard();
+		chooseCharacter();
+
+	}
 }
