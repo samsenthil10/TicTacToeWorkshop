41183ab1266f726f9c2ecfd761b4b1207ddf4a83
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-06 08:16:28 +0530

    Added: To initalize board grid

---
+++ src/com/bridgelabz/tictactoe/TicTacToe.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.tictactoe;
+
+public class TicTacToe {
+	
+	public char[] board= new char[11];
+	
+	public void initializeBoard() {
+		for(int index =1;index<11;index++) {
+			board[index] = ' ';
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
